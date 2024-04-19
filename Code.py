3488aa5a191ed04d4265d310397ccee31487a2f5
3Doge <161205486+3Doge@users.noreply.github.com>
2024-04-19 19:24:18 +0100

    Update Code.py
    added create directory (cd)

--- Code.py
+++ Code.py
@@ -1,5 +1,16 @@
 import os
 
+# ignore this it is just setup
+# setup for create directory
+def create_directory(directory_name):
+    try:
+        os.mkdir(directory_name)
+        print("Directory '{}' created successfully.".format(directory_name))
+    except FileExistsError:
+        print("Directory '{}' already exists.".format(directory_name))
+
+
+
 username = input("Baps name eg: BapsisGod: ")
 
 i = 0
@@ -15,5 +26,11 @@ while i <= 10:
     if text.lower() == "sd":
         new_directory = input("which directory?")
         os.chdir(new_directory)
+    # make directory
+    if text.lower() == "cd":
+        new_directory = input("Enter the name of the directory to create: ")
+        create_directory(new_directory)
 
         print("Current directory after changing to:", os.getcwd())
+
+
